633ab3714de3f523fa1455083bb95b564f331fe8
hey-now <rubinand22@gmail.com>
2022-10-28 11:31:24 -0700

    Session install

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session= require('express-session');
 var methodOverride = require('method-override');
 
 require('dotenv').config();
@@ -21,6 +22,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
