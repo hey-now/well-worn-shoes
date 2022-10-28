6a8b56f1c1669e78bea5877b1375c4ad1680e55f
hey-now <rubinand22@gmail.com>
2022-10-28 10:53:50 -0700

    Connect to database

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
+
+require('dotenv').config();
+require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
